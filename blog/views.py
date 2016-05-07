b36dbb12d983b0f36da6ee1c05bc94f0bcf39497
rui.xu <xurui2005@126.com>
2016-05-07 22:46:57 +0800

    add logging to setting

--- blog/views.py
+++ blog/views.py
@@ -2,10 +2,14 @@ from django.shortcuts import render, redirect
 from .models import Item
 from redis import Redis
 import os
+import logging
+
 redis = Redis(host=os.environ['REDIS_PORT_6379_TCP_ADDR'],
               port=os.environ['REDIS_PORT_6379_TCP_PORT'],
               password=os.environ.get('REDIS_PASSWORD'))
 
+#log
+logger = logging.getLogger(__name__)
 
 def home(request):
     if request.method == 'POST':
@@ -13,4 +17,5 @@ def home(request):
         return redirect('/')
     items = Item.objects.all()
     counter = redis.incr('counter')
+    logger.info(counter)
     return render(request, 'home.html', {'items': items, 'counter': counter})
--- myblog/settings.py
+++ myblog/settings.py
@@ -72,6 +72,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'myblog.wsgi.application'
 
+DAO_TEST = bool(os.environ.get('DAO_TEST'))
 
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
@@ -134,3 +135,67 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+# log config
+LOG_FILE = "./all.log"
+
+LOGGING = {
+        'version': 1,
+        'disable_existing_loggers': True,
+
+        'filters': {
+            'require_debug_false': {
+                '()': 'django.utils.log.RequireDebugFalse'
+                }
+            },
+        'formatters': {
+            'simple': {
+                'format': '[%(levelname)s] %(module)s : %(message)s'
+                },
+            'verbose': {
+                'format':
+                    '[%(asctime)s] [%(levelname)s] %(module)s : %(message)s'
+                }
+            },
+
+        'handlers': {
+            'null': {
+                'level': 'DEBUG',
+                'class': 'logging.NullHandler',
+                },
+            'console': {
+                'level': 'INFO',
+                'class': 'logging.StreamHandler',
+                'formatter': 'verbose'
+                },
+            'file': {
+                'level': 'INFO',
+                'class': 'logging.FileHandler',
+                'formatter': 'verbose',
+                'filename': LOG_FILE,
+                'mode': 'a',
+                },
+            'mail_admins': {
+                'level': 'ERROR',
+                'class': 'django.utils.log.AdminEmailHandler',
+                'filters': ['require_debug_false']
+                }
+            },
+        'loggers': {
+            '': {
+                'handlers': ['file', 'console'],
+                'level': 'INFO',
+                'propagate': True,
+                },
+            'django': {
+                'handlers': ['file', 'console'],
+                'level': 'DEBUG',
+                'propagate': True,
+                },
+            'django.request': {
+                'handlers': ['mail_admins', 'console'],
+                'level': 'ERROR',
+                'propagate': True,
+                },
+            }
+        }
