0741dcb6ab9451a80b6091260cedab8af3a5bea6
rui.xu <xurui2005@126.com>
2016-05-10 22:22:05 +0800

    fix deploy myblog to daocloud issue

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
 from django.db import models
@@ -8,3 +9,10 @@ from django.db import models
 class Item(models.Model):
     text = models.TextField(blank=False, null=False)
     date_posted = models.DateField(auto_now=True)
+
+
+class Category(models.Model):
+    pass
+
+class Article(models.Model):
+    pass
